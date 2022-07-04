4e499ce9e4a829ca62bd0c5836258e3dfb52d494
Roberto Dantas <7robertodantas@gmail.com>
2022-07-04 10:38:17 -0300

    Add national registry client

---
+++ third-party/src/main/kotlin/com/addi/thirdparty/NationalRegistryClient.kt
@@ -0,0 +1,28 @@
+package com.addi.thirdparty
+
+import com.addi.business.domain.PersonRegistry
+import com.addi.business.domain.command.GetPersonDataCommand
+import com.addi.business.domain.exceptions.PersonNotFoundException
+import com.addi.business.thirdparty.adapter.NationalRegistry
+import org.slf4j.LoggerFactory
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class NationalRegistryClient(
+    baseUrl: String
+) : BaseClient(baseUrl, logger, errorHandler), NationalRegistry {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(NationalRegistryClient::class.java)
+        private val errorHandler: ErrorHandler = { url, status, body ->
+            when(status) {
+                NOT_FOUND -> PersonNotFoundException("Could not perform request to get person at '$url'")
+                else -> Exception("Could not perform request to get person registry at '$url'. response '$body'")
+            }
+        }
+    }
+
+    override suspend fun getRegistry(command: GetPersonDataCommand): PersonRegistry {
+        return get("/persons/${command.nationalIdNumber}/registry")
+    }
+}
